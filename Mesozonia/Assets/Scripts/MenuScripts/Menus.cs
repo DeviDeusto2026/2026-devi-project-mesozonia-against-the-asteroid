9962b899eff5dfa316de527c69bab33ae77db655
murkri229 <mikel.urquijo@opendeusto.es>
2026-05-28 12:05:16 +0200

    In-Game OptionMenu
    In game Option menu opens when pressed the key escape

--- Mesozonia/Assets/Scripts/MenuScripts/Menus.cs
+++ Mesozonia/Assets/Scripts/MenuScripts/Menus.cs
@@ -1,30 +1,43 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Menus : MonoBehaviour
 {
 
-    public bool isActive = true;
+    public GameObject panelOptions;
+    public InputActionReference escape;
+
+    public bool isActive = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        escape.action.started += escapeAction;
     }
 
     // Update is called once per frame
     void Update()
     {
-        openOptionMenu();
     }
 
-    private void openOptionMenu() {
-        if (Input.GetKeyDown(KeyCode.Escape) && isActive == true)
-        {
-            this.gameObject.SetActive(false);
+    
+
+    void escapeAction(InputAction.CallbackContext context) {
+        Debug.Log("Se ha llamado");
+        if (isActive == false) {
+            Time.timeScale = 0;
+            Debug.Log("Activado");
+            panelOptions.SetActive(true);
+            
         }
-        if(Input.GetKeyDown(KeyCode.Escape) && isActive == false) {
-            this.gameObject.SetActive(true);
+        if (isActive == true) {
+            Time.timeScale = 1;
+            Debug.Log("Desactivado");
+            panelOptions.SetActive(false);
+            
         }
+
+        isActive = !isActive;
     }
 }
