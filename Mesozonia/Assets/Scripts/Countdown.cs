24318a8b3e46a8db977b41d1f7421730d7f2ee32
murkri229 <mikel.urquijo@opendeusto.es>
2026-05-29 11:48:25 +0200

    Prefabs, cowntdown & pieces
    Character prefabs
    Cowntdown function and implementation
    Pieces function and implementation correct

---
+++ Mesozonia/Assets/Scripts/Countdown.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    [SerializeField] Text countDownText;
+    private float seconds = 30;
+    private float minutes = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //countDown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (seconds <= 0) {
+            seconds = 60;
+            minutes--;
+        }
+
+        seconds -= Time.deltaTime;
+
+        Debug.Log("Minutos: " + minutes);
+        Debug.Log("Segundos: " + seconds);
+
+        int seconds2 = (int)seconds;
+        countDownText.text = "TIME LEFT: " + minutes.ToString() + " : " + seconds2.ToString();
+
+        if (minutes <= 0 && seconds <= 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
+    private void countDown() {
+        while (minutes > 0) {
+            minutes--;
+            Debug.Log("Minutos: " + minutes);
+            while (seconds > 0) {
+                Debug.Log("Segundos: " + seconds);
+                seconds -= Time.deltaTime;
+                countDownText.text = "TIME LEFT: " + minutes.ToString() + " : " + seconds.ToString();
+            }
+           
+            seconds = 60;
+        }
+
+        if (minutes == 0) {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
